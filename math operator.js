6bcb9629f5a9645eb1d20b80b7ba36027db747fc
Surojit Bisai <surojitbisai8328@gmail.com>
2026-09-17 11:33:44 +0530

    Math Operator in JavaScript

---
+++ math operator.js
@@ -0,0 +1,4 @@
+const width = prompt('Plice enter rectangle width')
+const height = prompt('Plice enter rectangle height')
+console.log(width * height);
+document.write(`Rectangle Area: ${width * height}`)
